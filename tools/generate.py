882e63e16d5b0d126b1aa6845783719ca3839bdd
patrik999 <patrik999@gmail.com>
2014-04-15 23:06:50 +0200

    Inital versions of generation tools

---
+++ tools/generate.py
@@ -0,0 +1,556 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import getopt, sys
+import optparse
+import psycopg2
+import shlex, subprocess
+import collections
+import os
+import imp
+from abc import ABCMeta
+
+import spatialRelationsReader
+
+KeyValue = collections.namedtuple('KeyValue', ['key', 'value'])
+
+# Different dictionaries for connections, files, scripts
+dictConnections = {}
+dictFiles = {}
+dictScripts = {}
+dictConstants = {}
+listMappings = []
+
+
+class Mapping:
+
+    id = ""
+    mapType = 0
+    sourceId = ""
+    sourceQuery = ""
+
+    transformId = ""
+    transformQuery  = ""
+
+    targetId = ""
+    targetOutput = ""
+    
+    dependency = ""
+
+    def __init__(self, id):
+        self.id = id
+    
+    def display(self):
+        print "Id: ", self.id
+        print "Source: ", self.sourceId, self.sourceQuery
+        print "Transform: ", self.transformId, self.transformQuery
+        print "Target: ", self.targetId, self.targetOutput
+
+
+# Base class for a reader
+class Reader:  #__metaclass__ = ABCMeta
+
+    connectionString = ""
+    mapObj = None
+
+    #@property
+    #def mapObj(self):
+    #    return self.__mapObj
+    
+    def __init__(self, mapping, connection):
+        self.mapObj = mapping
+        self.connectionString = connection
+    
+
+    def open(self):
+        # Here comes the open
+        print "Source opened"
+
+    def read(self):
+        # Here comes the read and yield
+        print "Source read"
+
+    def close(self):
+        # Here comes the close
+        print "Source closed"
+        
+
+class FileReader(Reader):
+
+
+    sepChar = ''
+    fileName = ""
+
+    @staticmethod
+    def extractFileNameSeparator(fileString):
+        # 2 cases with delimiter or without
+        posName = fileString.find("name=")
+        posSep = fileString.find("delimiter=")
+        sepCharRet = ''
+        fileNameRet = ""
+        
+        if posSep < 0:
+            fileNameRet = fileString[(posName+5):]
+        else:
+            fileNameRet = fileString[(posName+5):(posSep-1)]
+            sepCharRet = fileString[(posSep+11):(posSep+12)]
+
+        return (fileNameRet, sepCharRet)
+
+                
+    def open(self):
+        
+        (name, sep) = FileReader.extractFileNameSeparator(self.connectionString)
+        self.fileName = name
+        self.sepChar = sep
+
+    
+    def read(self):
+        
+        with open(self.fileName, 'r') as f1:
+
+            lines = f1.readlines()
+            for i in range(len(lines)):
+                tuple_file = lines[i]
+                if posDel > -1:
+                    yield (tuple_file,) #  
+                else:
+                    tempList = tuple_file.split(sepChar)
+                    tuple_sep = tuple(tempList)
+                    yield tuple_sep
+                #print tuple_file
+
+        f1.closed
+
+#def close():
+         
+
+class DatabaseReader(Reader):
+
+    conn_gis = None
+    cur_gis = None
+    
+    def open(self):
+
+        # Connect to a PostGIS database and open a cursor to perform database operations
+        self.conn_gis = psycopg2.connect(self.connectionString)
+        # Set DB parameter
+        self.conn_gis.set_client_encoding('UTF8')
+        self.cur_gis = self.conn_gis.cursor()
+
+        
+    def read(self):
+
+        self.cur_gis.execute(self.mapObj.sourceQuery)
+        
+        for tuple_gis in self.cur_gis:
+
+            #tupleOut = self.mapObj.targetOutput
+            
+            yield tuple_gis
+
+    def close(self):
+
+        self.cur_gis.close()
+        self.conn_gis.close()
+
+        
+class ScriptReader(Reader):
+
+    scriptReader  = None
+
+    def open(self, input):
+
+        self.scriptReader = self.load_from_file(self.connectionString)
+        self.scriptReader.open(input)
+ 
+        
+    def read(self):
+        
+        for line in self.scriptReader.read():
+            yield  line
+            
+
+    def close(self):
+
+        scriptReader.close()
+        
+    def load_from_file(self, filepath):
+        class_inst = None
+        expected_class = "CustomScriptReader"
+
+        mod_name,file_ext = os.path.splitext(os.path.split(filepath)[-1])
+        #sReader = spatialRelationsReader.CustomScriptReader()
+        
+        #if os.path.exists(filepath):
+        #print "Exists"
+        
+        if file_ext.lower() == '.py':
+
+            py_mod = imp.load_source(mod_name, filepath)
+
+        elif file_ext.lower() == '.pyc':
+            py_mod = imp.load_compiled(mod_name, filepath)
+
+        if hasattr(py_mod, expected_class):
+            class_inst = getattr(py_mod, expected_class)()
+
+        return class_inst
+
+
+class ConstantReader(Reader):
+
+    listIDs = []
+    constants = {}
+    
+    def __init__(self, mapping, consts):
+
+        self.mapObj = mapping
+        self.constants = consts
+        #self.connectionString = connection
+        #print consts
+ 
+    def open(self):
+
+        self.listIDs = self.mapObj.sourceQuery.split(' ')
+    
+    def read(self):
+
+        for index in range(len(self.listIDs)):
+            yield (self.constants[self.listIDs[index]],)
+            
+
+# Base class for a writer
+class Writer:
+
+    connectionString = ""
+    mapObj = None
+
+    def __init__(self, mapping, connection):
+        self.mapObj = mapping
+        self.connectionString = connection
+
+    def open(self):
+        print "Target opened"
+            
+    def write(self, tuple):
+        print "Target written"
+        
+    def close(self):
+        print "Target closed"
+        
+        
+class FileWriter(Writer):
+
+    textfile = None
+    fileName = ""
+    sepChar = ''
+        
+    def open(self):
+        
+        (name, sep) = FileReader.extractFileNameSeparator(self.connectionString)
+        self.fileName = name
+        self.sepChar = sep
+                    
+        self.textfile = open(self.fileName, 'a') # w+
+
+    def write(self, tuple):
+        
+        self.textfile.write(tuple + '\n')
+        
+
+    def close(self):
+
+        self.textfile.closed
+
+
+
+#class DatabaseWriter(Writer):
+
+    #def open(self):
+            
+    #def write(self, tuple):
+
+    #def close(self):
+    
+        
+def main(argv):
+
+    # Parse command line input
+    parser = optparse.OptionParser()
+    parser.add_option('--mappingFile', action="store", dest="mapFile", default="")
+    parser.add_option('--verbose', action="store_true", dest="verbose", default=False)
+    (options, args) = parser.parse_args()
+
+    # Read config file
+    with open(options.mapFile, 'r') as f1:
+        lines = f1.readlines()
+
+        createConnections(lines)
+        createMappings(lines)
+
+    f1.closed
+
+    # Loop Mapping and run them
+    for i in range(len(listMappings)):
+        mapObj = listMappings[i]
+        #mapObj.display()
+
+        runMapping(mapObj)
+
+def runMapping(mapObj): 
+
+    fileName = ""
+    dataWriter = None
+    dataReader  = None
+
+    # Main method for ETLing the data
+    print "Transfering " +  mapObj.id
+    
+    # Create DB/File/Script reader (extract)
+    if mapObj.sourceId in dictConnections:
+
+        dataReader = DatabaseReader(mapObj, dictConnections[mapObj.sourceId])
+        dataReader.open()
+        
+    elif mapObj.sourceId in dictFiles:
+
+        dataReader = FileReader(mapObj, dictFiles[mapObj.sourceId])
+        dataReader.open()
+
+    elif mapObj.sourceId in dictScripts:
+
+        dataReader = ScriptReader(mapObj, dictScripts[mapObj.sourceId])
+        scriptInput = mapObj.sourceQuery
+        tempArgs = scriptInput.split(' ')
+
+        for i in range(len(tempArgs)):
+            #if tempArgs[i].find("{") >= 0:
+            tempID = tempArgs[i]
+            #tempID = tempID[1:len(tempID)-1]
+            if tempID in dictFiles:
+                (fileName, sep) = FileReader.extractFileNameSeparator(dictFiles[tempID])
+                scriptInput = scriptInput.replace(tempArgs[i], fileName)
+        
+        dataReader.open(scriptInput)
+        
+    elif mapObj.sourceId.find("const") >= 0:
+
+        dataReader = ConstantReader(mapObj, dictConstants)
+        dataReader.open()
+        
+    # Create DB/File writers (load)
+    if mapObj.targetId in dictConnections:
+
+        dataWriter = DatabaseWriter(mapObj, dictConnections[mapObj.targetId])
+        dataWriter.open()
+
+    elif mapObj.targetId in dictFiles:
+
+        dataWriter = FileWriter(mapObj, dictFiles[mapObj.targetId])
+        dataWriter.open()
+        
+
+    if dataReader == None:
+        print mapObj.id + ": Reader can not be created"
+        return
+
+            
+    # Iterate with reader, using yield
+    for tuple in dataReader.read():
+
+        #print tuple
+        #Replace values of read tuples in result string
+        tupleOut = mapObj.targetOutput
+        
+        for i in range(len(tuple)):
+            repID = '{' + str(i+1) + '}'  # We assume that the indexes start with 
+            posID = tupleOut.find(repID)
+            posValue = str(tuple[i])
+            if posID > -1:
+                posValue = posValue.replace('"', '')  # Clean up data and remove double quote, as it causes with some turtle readers
+                tupleOut = tupleOut.replace(repID, posValue)
+            
+        # Write (only if it has some text)
+        #print tupleOut
+        if(len(tupleOut) > 0):
+            dataWriter.write(tupleOut)
+
+
+    dataWriter.close()
+    dataWriter.open()
+
+
+        
+def createConnections(lines):
+
+        # Read mapping file for connections, files, scripts, prefix, base, imports
+        for i in range(len(lines)):
+            nline = lines[i].rstrip()
+            
+            posConn = nline.find("CONNECTION")
+            if posConn > -1: # CONNECTION osm1: host=localhost port=5432 dbname=osm_vienna user=postgres password=ps
+                kv = getKeyValueColon(nline)
+                dictConnections[kv.key] = kv.value
+           
+            posFile = nline.find("FILE")
+            if posFile > -1: # FILE f1: /Users/patrik/Desktop/OSM_Bench/temp.rdf
+                kv = getKeyValueColon(nline)
+                dictFiles[kv.key] = kv.value.strip()
+
+            posScript = nline.find("SCRIPT")
+            if posScript > -1: # SCRIPT s1: /Users/patrik/Desktop/OSM_Bench/extractRelation.py --relation {1} --role {2} --input1 {3} --input2 {4}
+                kv = getKeyValueColon(nline)
+                dictScripts[kv.key] = kv.value.strip()
+
+            posPrefix = nline.find("CONST")
+            if posPrefix > -1: # CONST tuwt: <http://www.kr.tuwien.ac.at/myits/geoconcepts/terms#>
+                kv = getKeyValueColon(nline)
+                dictConstants[kv.key] = kv.value.strip()
+
+                
+def createMappings(lines):
+
+        # Read file for mappings
+        actMapID = savMapID = ""
+        mappingLines = []
+                                        
+        for i in range(len(lines)):
+            nline = lines[i]
+            #print nline
+            
+            # The mappingID is the indicator, when a single mapping is finished, new id -> new mapping
+            posImport = nline.find("mappingId")
+            if posImport > -1:
+                kv = getKeyValueSpace(nline)
+                actMapID = kv.value
+                 
+                if actMapID != savMapID and len(savMapID) > 0:
+                    mapObj = createMappingObj(savMapID, mappingLines)
+                    listMappings.append(mapObj)
+                    
+                    mappingLines = []
+
+                savMapID = actMapID
+                    
+            # Simply keep all mapping lines in an array
+            if len(actMapID) > 0:
+                mappingLines.append(nline)
+
+
+        if len(actMapID) > 0:
+            mapObj = createMappingObj(actMapID, mappingLines)
+            listMappings.append(mapObj)
+
+
+def createMappingObj(id, lines):
+
+    linesForType = []
+    typeOfLine = saveType = 0   # 1 = Source, 2 = Transform, 3 = Target, 4 = Dependency
+
+    
+    mapObj = Mapping(id.rstrip())
+
+    for i in range(len(lines)):
+        nline = lines[i]
+        
+        posSource = nline.find("source")
+        if posSource > -1:
+           typeOfLine = 1
+
+        posTrans = nline.find("transform")
+        if posTrans > -1:
+           typeOfLine = 2
+
+        posTarget = nline.find("target")
+        if posTarget > -1:
+           typeOfLine = 3
+
+        posDep = nline.find("dependency")
+        if posDep > -1:
+           typeOfLine = 4
+
+        if saveType != typeOfLine and saveType > 0:
+           #print saveType, linesForType
+           createMappingPart(mapObj, saveType, linesForType)
+           linesForType = []
+
+        if typeOfLine > 0:
+           linesForType.append(nline)
+
+           
+        saveType = typeOfLine
+        
+
+    if saveType > 0:
+        #print saveType, linesForType
+        createMappingPart(mapObj, saveType, linesForType)
+
+    return mapObj
+        
+def createMappingPart(mapObj, type, lines):
+
+    tempId = ""
+    tempText = "" 
+
+    mapObj.mapType = type
+
+    
+    for i in range(len(lines)):
+        nline = lines[i]
+
+        # First line is different and the standard case
+        if i == 0 and type != 4:
+           # Input has the structure TYPE ID TEXT....
+           # We skip the first token TYPE and use ID and TEXT
+           kv1 = getKeyValueSpace(nline)
+           kv2 = getKeyValueSpace(kv1.value)
+           tempId = kv2.key
+           tempText = kv2.value
+        else:
+           tempText = tempText + nline
+    
+    
+    if type == 1:
+        mapObj.sourceId = tempId
+        mapObj.sourceQuery = tempText.rstrip()
+    elif type == 2:
+        mapObj.transformId = tempId
+        mapObj.transformQuery = tempText.rstrip()
+    elif type == 3:
+        mapObj.targetId = tempId
+        mapObj.targetOutput = tempText.rstrip()
+        #print type, tempId, tempText
+    elif type == 4:
+        mapObj.dependency = tempText.rstrip()
+   
+
+def getKeyValueSpace(text):
+
+    p = text.find(" ")
+    if p > 0:
+        kv = KeyValue(text[:p], text[p+1:])
+    else:
+        kv = KeyValue("","")
+    
+    return kv
+
+def getKeyValueColon(text):
+
+    p = text.find(":")
+    if p > 0:
+        type_id = text[0:p] .split(' ')
+        kv = KeyValue(type_id[1], text[p+1:]) 
+    else:
+        raise NameError('Incorrect syntax in line ', i)
+
+    return kv
+
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1:])
+
+
+
+
---
+++ tools/spatialRelationsReader.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import getopt, sys
+import optparse
+from math import * 
+
+from osgeo import ogr
+from shapely import *
+from shapely.geometry import *
+from shapely.wkt import *
+#from shapely.wkb import *
+
+Buffer_Distance_Iso = 0.01 #  0.001
+Buffer_Quadrant_Segments = 3
+
+gml_name = "gml:featurename"
+rdf_type = "rdf:type"
+geo_point = "geo:point"
+geo_line = "geo:line"
+geo_poly = "geo:polygon"
+
+#prefix_osm_points = "110"
+
+class CustomScriptReader():
+
+    relation = ""
+    role = ""
+    input1 = ""
+    input2 = ""
+
+    def open(self, parameter):
+
+        vals = parameter.split(' ')
+
+        self.relation = vals[0]
+        #self.role = vals[1]
+        self.input1 = vals[1]
+        self.input2 = vals[2]
+
+        if self.input2 is None or len(self.input2) == 0: self.input2 = self.input1
+
+
+    def read(self):
+
+        #results1 = extract_id_geo(input1)
+        #results2 = extract_id_geo(input2)
+
+        #for i in range(len(results1)-1):
+        print self.input1, self.input2
+        for nline1 in self.extract_id_geo(self.input1):
+
+            #nline1 = results1[i]
+            temp1 = nline1.split('|')
+            obj_id1 = temp1[0]
+            obj_geo1 = temp1[1]
+
+            print "Current: ",  obj_id1
+            #geo1 = ogr.CreateGeometryFromWkt(obj_geo1) # res.wkt
+            geo1 = loads(obj_geo1)                                 
+            if geo1 is None: continue
+
+            for nline2 in self.extract_id_geo(self.input2):
+            #for j in range(len(results2)-1):
+                
+                #snline2 = results2[j]
+                temp2 = nline2.split('|')
+                obj_id2 = temp2[0]
+                obj_geo2 = temp2[1]
+
+                if obj_id1 == obj_id2: continue
+
+                geo2 = loads(obj_geo2)            
+                if geo2 is None: continue
+
+                #print "Current: ", obj_id1, obj_id2
+
+                # Heuristics to speed up, only consider objects which are in a radius of 100 km (for large objects take the upper left corner)
+                lon1 = 0.0; lat1 = 0.0; lon2 = 0.0; lat2 = 0.0;
+                (lat1, lon1, maxx1, maxy1) = geo1.bounds
+                (lat2, lon2, maxx2, maxy2) = geo2.bounds
+
+                distance = self.haversine(lon1, lat1, lon2, lat2)
+                #print distance
+                if distance > 50: continue
+
+                if self.relation == "contains":
+                    #print geo1.bounds, geo2.bounds
+                    if geo1.contains(geo2):
+                        #print obj_id1, self.role, obj_id2
+                        yield (obj_id1, obj_id2)
+                elif self.relation == "within":
+                    if geo1.within(geo2):
+                        yield (obj_id1, obj_id2)
+                elif self.relation == "crosses":
+                    if geo1.crosses(geo2):
+                        yield (obj_id1, obj_id2)
+                elif self.relation == "disjoint":
+                    if geo1.disjoint(geo2):
+                        yield (obj_id1, obj_id2)
+                elif self.relation == "equals":
+                    if geo1.equals(geo2):
+                        yield (obj_id1, obj_id2)
+                elif self.relation == "intersects":
+                    if geo1.intersects(geo2):
+                        yield (obj_id1, obj_id2)
+                elif self.relation == "touches":
+                    if geo1.touches(geo2):
+                        yield (obj_id1, obj_id2)
+                elif self.relation == "next":
+                    temp_geo = geo1.buffer(Buffer_Distance_Iso) #  Buffer_Distance_Iso, Buffer_Quadrant_Segments)¶
+                    if temp_geo.contains(geo2): # Need to change this
+                        yield (obj_id1, obj_id2)
+
+    #def close():
+
+
+    def haversine(self, lon1, lat1, lon2, lat2):
+        #
+        #Calculate the great circle distance between two points 
+        #on the earth (specified in decimal degrees)
+        #
+        
+        # convert decimal degrees to radians 
+        lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+        # haversine formula 
+        dlon = lon2 - lon1 
+        dlat = lat2 - lat1 
+        a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+        c = 2 * asin(sqrt(a)) 
+        km = 6367 * c
+        return km
+
+
+
+    def extract_id_geo(self, file_name):
+
+        #results = []
+
+        with open(file_name, 'r') as f1:
+
+            actID = actGeo = ""
+            lines = f1.readlines()
+            for i in range(len(lines)-1):
+                nline = lines[i]
+
+                posID = nline.find(rdf_type)
+                if posID > 0: # tuwi:24820994 rdf:type tuwt:Playground, owl:NamedIndividual;
+                    actID = nline[0:posID-1]
+
+                posGeo = -1
+                posGeo = nline.find(geo_point)
+                if posGeo == -1:
+                    posGeo = nline.find(geo_line)
+                    if posGeo == -1:
+                        posGeo = nline.find(geo_poly)
+                        if posGeo == -1:
+                            posGeo = nline.find(geo_line)
+                if posGeo >= 0: #  geo:polygon "0103000020E610000"
+                    posGeo2 = nline.find('\"')
+                    posGeo3 = nline.find('^')
+
+                    actGeo = nline[posGeo2+1: posGeo3-1]
+                    temp3 = actGeo.split(';') # Remove SRID=4326;
+                    #results.append(actID + '|' + temp3[1])
+                    if len(temp3) > 1:
+                        yield actID + '|' + temp3[1]
+
+
+
+            f1.closed
+
+        #return results
+
+
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1:])
